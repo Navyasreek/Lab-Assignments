6b96c75c4fe8e7b92551b6ead908bc3d063ea439
Navyasreek <navyasree.ksr16@gmail.com>
2017-03-16 01:26:38 -0500

    committed

--- lab assignment 8/Source/www/js/services.js
+++ lab assignment 8/Source/www/js/services.js
@@ -1,92 +1,81 @@
-var app = angular.module('draw.services',[]);
+angular.module('starter.services', [])
+    .service('LoginService', function($q) {
+      return {
+        loginUser: function(name, pw) {
+          var deferred = $q.defer();
+          var promise = deferred.promise;
 
-/**
-* Factory: Search Data Services using $resource
-*/
-app.factory('DataService', function( $resource, nutritionixConst){
-  var aSearchObject = $resource('https://api.nutritionix.com/v1_1/search/:term',{term: '@term'},{
-    getAll : {
-      method : 'get',
-      //isArray : true,
-      params : {
-        results  : ':results',
-        appId : nutritionixConst.appId,
-        appKey  :nutritionixConst.appKey,
-        // brand_id:'513fbc1283aa2dc80c00001f',
-        fields : ':fields',
-      }
-    }
-  });
-  return {
-    /**
-    * we can specify the params, the query string and the default fields
-    * to turn in the query along with the result size
-    */
-    getAll : function(_params) {
-      var defaultFields = 'brand_id,item_name,item_id,brand_name,nf_calories,nf_total_fat';
-
-      if (!_params.fields) {
-        _params.fields = defaultFields;
-      }
-      return aSearchObject.getAll(_params);
-    }
-  }
-
-});
-
-/**
-* Factory: Search Data Services using $http
-*/
-app.factory('DataServiceHTTP', function( $http, nutritionixConst){
-  return {
-    getAll : function(_key) {
-
-      return $http.get('https://api.nutritionix.com/v1_1/search/' + _key,{
-        'params' : {
-          results  : '0:50',
-          appId : nutritionixConst.appId,
-          appKey  :nutritionixConst.appKey,
-          fields : 'brand_id,item_name,item_id,brand_name,nf_calories,nf_total_fat'
+          if (name == 'user' && pw == 'secret') {
+            deferred.resolve('Welcome ' + name + '!');
+          } else {
+            deferred.reject('Wrong credentials.');
+          }
+          promise.success = function(fn) {
+            promise.then(fn);
+            return promise;
+          }
+          promise.error = function(fn) {
+            promise.then(null, fn);
+            return promise;
+          }
+          return promise;
         }
-      });
-    }
-  }
-});
+      }
+    })
 
-/**
-* Factory: View data using $http
-*/
-app.factory('ViewDataServiceHTTP', function( $http, nutritionixConst){
+
+
+
+
+
+
+
+
+.factory('Chats', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var chats = [{
+    id: 0,
+    name: 'Ben Sparrow',
+    lastText: 'You on your way?',
+    face: 'img/ben.png'
+  }, {
+    id: 1,
+    name: 'Max Lynx',
+    lastText: 'Hey, it\'s me',
+    face: 'img/max.png'
+  }, {
+    id: 2,
+    name: 'Adam Bradleyson',
+    lastText: 'I should buy a boat',
+    face: 'img/adam.jpg'
+  }, {
+    id: 3,
+    name: 'Perry Governor',
+    lastText: 'Look at my mukluks!',
+    face: 'img/perry.png'
+  }, {
+    id: 4,
+    name: 'Mike Harrington',
+    lastText: 'This is wicked good ice cream.',
+    face: 'img/mike.png'
+  }];
+
   return {
-    getAll : function(_key) {
-
-      return $http.get('https://api.nutritionix.com/v1_1/item?id=' + _key,{
-        'params' : {
-          appId : nutritionixConst.appId,
-          appKey  :nutritionixConst.appKey
+    all: function() {
+      return chats;
+    },
+    remove: function(chat) {
+      chats.splice(chats.indexOf(chat), 1);
+    },
+    get: function(chatId) {
+      for (var i = 0; i < chats.length; i++) {
+        if (chats[i].id === parseInt(chatId)) {
+          return chats[i];
         }
-      });
+      }
+      return null;
     }
-  }
-});
-
-
-/**
-* Factory: Weather
-*/
-app.factory('Weather', function($resource) {
-
-  var API_PATH = 'http://api.openweathermap.org/data/2.5/weather';
-
-  var Weather = $resource(API_PATH);
-
-  return {
-    getWeather: function(weatherParams) {
-      return Weather.get(weatherParams, function(successResult) {
-        return successResult;
-      }, function(errorResult) {
-        console.log(errorResult);
-      });
-    }
-  }
+  };
 });
